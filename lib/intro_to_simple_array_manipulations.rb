7c255f1cd1e7287f833fddf64862b8175819d6bb
Adan Escamilla <adanescamilla580@gmail.com>
2020-12-24 07:05:05 +0000

    Done.

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,30 @@
+require 'pry'
+
+def using_concat(array, string)
+  array.concat(string)
+end
+
+def using_insert (array, string)
+array.insert(4, string)
+end
+
+def using_uniq (array)
+  array.uniq
+end
+
+def using_flatten (array)
+  array.flatten
+end
+
+def using_delete (array, string)
+  array.delete(string)
+end
+
+def using_delete_at (array, string)
+  array.delete_at(string)
+end
+
+
+
+
+
